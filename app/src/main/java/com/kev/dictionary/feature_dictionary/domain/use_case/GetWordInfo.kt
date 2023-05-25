6a534b15a0e65e390d610051020add91eaa4320d
kev87ian <kelvinian87@gmail.com>
2023-05-25 15:15:59 +0300

    wrote the usecase

---
+++ app/src/main/java/com/kev/dictionary/feature_dictionary/domain/use_case/GetWordInfo.kt
@@ -0,0 +1,20 @@
+package com.kev.dictionary.feature_dictionary.domain.use_case
+
+import com.kev.dictionary.feature_dictionary.core.util.Resource
+import com.kev.dictionary.feature_dictionary.domain.model.WordInfo
+import com.kev.dictionary.feature_dictionary.domain.repository.WordInfoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetWordInfo @Inject constructor(
+    private val repository: WordInfoRepository
+) {
+     operator fun invoke(word: String): Flow<Resource<List<WordInfo>>>{
+         if (word.isBlank()){
+             return flow {  }
+         }
+
+         return repository.getWordInfo(word)
+     }
+}
